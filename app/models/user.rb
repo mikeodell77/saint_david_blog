258fa4064fd0d37aae3eb189a9f906782bbc9303
Mike O'Dell <mike@gift-give.com>
2013-01-05 10:17:47 -0500

    Fixing landing page to show date of last blog and add login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :blog_entries, :image, :bio
   # attr_accessible :title, :body
 
   has_many :blog_entries
   mount_uploader :image, ImageUploader
+
+
+  def get_last_blog_entry
+  	blogEntry = self.blog_entries.last
+  end
 
   def bio_appender
   	self.bio
